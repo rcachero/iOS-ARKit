f9af9ec14e255851463eed510b7010d9f25a71fa
Ralph Cachero <ralph.cachero@gmail.com>
2017-07-23 22:25:50 -0700

    Add pipe ramp for ramp picker vc

--- ramp-up/ramp-up/Controller/RampPlacerVC.swift
+++ ramp-up/ramp-up/Controller/RampPlacerVC.swift
@@ -77,4 +77,17 @@ class RampPlacerVC: UIViewController, ARSCNViewDelegate, UIPopoverPresentationCo
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+    @IBAction func onRampBtnPressed(_ sender: UIButton) {
+        let rampPickerVC = RampPickerVC(size: CGSize(width: 250, height: 500))
+        rampPickerVC.modalPresentationStyle = .popover
+        rampPickerVC.popoverPresentationController?.delegate = self
+        present(rampPickerVC, animated: true, completion: nil)
+        rampPickerVC.popoverPresentationController?.sourceView = sender
+        rampPickerVC.popoverPresentationController?.sourceRect = sender.bounds
+    }
 }
